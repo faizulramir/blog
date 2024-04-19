9db0ec65c3ca33d17e10f6e03e8197311ba146e5
Faizul Amir <faizul.ramir@gmail.com>
2024-04-19 21:32:35 +0800

    first

---
+++ resources/js/Components/ApplicationLogo.tsx
@@ -0,0 +1,7 @@
+import { SVGAttributes } from 'react';
+
+export default function ApplicationLogo(props: SVGAttributes<SVGElement>) {
+    return (
+        <img src="/favicon.png" alt="" />
+    );
+}
